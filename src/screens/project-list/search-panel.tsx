876728cc017c5dc81c364d9247968e7f399333b5
Akashi2019 <657318304@qq.com>
2021-04-05 10:29:56 +0800

    chore: add Context

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export interface User {
   id: string;
@@ -6,33 +6,49 @@ export interface User {
   email: string;
   title: string;
   organization: string;
+  token: string;
 }
 
 interface SearchPanelProps {
-  users: User[],
+  users: User[];
   param: {
     name: string;
     personId: string;
-  },
-  setParam: (param: SearchPanelProps['param']) => void
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
 }
-export const SearchPanel = ({users, param, setParam}: SearchPanelProps) => {
-  return <form>
-    <div>
-      {/* setParam(Object.assign({}, param, {name:evt.target.value})) */}
-      <input type="text" value={param.name} onChange={evt => setParam({
-        ...param,
-        name:evt.target.value
-      })}/>
-      <select value={param.personId} onChange={evt => setParam({
-        ...param,
-        personId: evt.target.value
-      })}>
-        <option value={''}>负责人</option>
-        {
-          users.map(user => <option value={user.id} key={user.id}>{user.name}</option>)
-        }
-      </select>
-    </div>
-  </form>
-}
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
+  return (
+    <form>
+      <div>
+        {/* setParam(Object.assign({}, param, {name:evt.target.value})) */}
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option value={user.id} key={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
